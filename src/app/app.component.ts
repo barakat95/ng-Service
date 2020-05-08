424cda5f1cac83aaeef8f82416814a9000bb0634
barakat95 <mohammedbarakat75@gmail.com>
2020-05-09 01:55:45 +0200

    Working with services angular 9

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+import { AccountsService } from "./accounts.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit{
+  constructor(private accountsService: AccountsService){}
+  accounts: { name: string; status: string }[] = [];
+  ngOnInit(): void {
+    this.accounts = this.accountsService.accounts;
+  }
+
+}
